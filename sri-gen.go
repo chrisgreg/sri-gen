92c5b211032efe3fa7f6d329040154332d183ea3
Christopher Gregori <christopher.gregori@net-a-porter.com>
2017-05-05 15:10:24 +0100

    Basic 512, 384 and 256 SHA hashing

--- sri-gen.go
+++ sri-gen.go
@@ -4,13 +4,20 @@ import (
   "fmt"
   "os"
   "io/ioutil"
-  "crypto/sha256"
+  "flag"
+  "crypto/sha512"
 )
 
 func main() {
   filePath := os.Args[1]
+  
+
+
+
+
   fileContents := readFile(filePath)
-  create256Hash(fileContents)
+  hash := create512Hash(fileContents)
+  fmt.Printf("%x", hash)
 }
 
 func readFile(path string) string {
@@ -19,10 +26,22 @@ func readFile(path string) string {
   return string(content)
 }
 
-func create256Hash(data string) {
-  hash := sha256.New()
+// func create256Hash(data string) []uint8 {
+//   hash := sha256.New()
+//   hash.Write([]byte(data))
+//   return hash.Sum(nil)
+// }
+
+// func create384Hash(data string) []uint8 {
+//   hash := sha512.New384()
+//   hash.Write([]byte(data))
+//   return hash.Sum(nil)
+// }
+
+func create512Hash(data string) []uint8 {
+  hash := sha512.New()
   hash.Write([]byte(data))
-  fmt.Printf("%x", hash.Sum(nil))
+  return hash.Sum(nil)
 }
 
 func check(e error) {
